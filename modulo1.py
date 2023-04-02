4e726c18afa3563c62a40738789e0a83e382a0d9
johnfernandob <68911624+johnfernandob@users.noreply.github.com>
2023-04-02 11:41:18 -0500

    Creado mediante Colaboratory

---
+++ modulo1.py
@@ -0,0 +1,51 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPsmeT34Nu7Jp6UzxcZGpl+",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/johnfernandob/Proyecto1/blob/master/modulo1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "oVUJbjss1m3a"
+      },
+      "outputs": [],
+      "source": [
+        "def sumar(a, b):\n",
+        "    return a + b\n",
+        "\n",
+        "def restar(a, b):\n",
+        "    return a - b\n",
+        "\n",
+        "def mult(a, b):\n",
+        "    return a * b\n",
+        "\n",
+        "def div(a, b):\n",
+        "    return a / b"
+      ]
+    }
+  ]
+}
